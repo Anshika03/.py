662b71ec85307aeb281808818fc3ef76786e72f7
Anshika Sharma <39482615+Anshika03@users.noreply.github.com>
2018-12-23 16:42:18 +0530

    Add files via upload

---
+++ vowel or consonant.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Anshika
+#
+# Created:     22-12-2018
+# Copyright:   (c) Anshika 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def fun():
+     ch = input("enter a value")
+     str="aeiouAEIOU"
+     print (ch,"is vowel:-",ch in str)
+
+
+if __name__ == '__main__':
+    fun()
